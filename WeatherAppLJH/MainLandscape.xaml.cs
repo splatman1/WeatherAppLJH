d01fe60c0fba5a6873d613cd80d56dd6ae95c702
Lachlan Haigh <lachlanljh@outlook.com>
2022-10-25 11:23:08 +0800

    Add project files.

---
+++ WeatherAppLJH/MainLandscape.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherAppLJH
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainLandscape : ContentView
+    {
+        public MainLandscape(string importantData)
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WeatherAppLJH/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeatherAppLJH
+{
+    public partial class MainPage : ContentPage
+    {
+
+        //previous screen size
+        private double width = 0;
+        private double height = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            UpdateLayout();
+        }
+
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height); //This must stay
+
+            if(this.width != width || this.height != height)
+            {
+                this.width = width;
+                this.height = height;
+
+                //screen size has changed
+                UpdateLayout();
+            }
+
+        }
+        /// <summary>
+        /// Decide on Portrait or Landscape layout
+        /// </summary>
+        private void UpdateLayout()
+        {
+            if(Content == null) return;
+            string inputString = Content.FindByName<Entry>("input")?.Text ?? "";
+
+            if (width > height)
+            {
+                //landscape
+                Content = new MainLandscape(inputString); 
+            }
+            else
+            {
+                //portrait
+                Content = new MainPortrait(inputString);
+            }
+
+        }
+
+    }
+}
